f3d71e0daaf5de6a651fa4477cca301fba67cb79
Hunter Bashaw <hunter@blivet.sh>
2019-12-05 18:32:46 -0500

    Initial commit

---
+++ gustard.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func handler(client net.Conn) {
+
+}
+
+func main() {
+	fmt.Println("Starting server...")
+	ln, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		fmt.Println("Error: port already in use")
+		return
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error: failed to connect to incoming client")
+			continue
+		}
+
+		go handler(conn)
+	}
+
+}
