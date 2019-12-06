09bcfc5b6b2d5a4b0537332d0d1a0988bd621c2c
Joshua-Gordon <joshua.sam.gordon@gmail.com>
2019-12-06 15:11:06 -0500

    Begin generating generator without knowing the language

--- elgamel.go
+++ elgamel.go
@@ -17,8 +17,14 @@ type PrivKey struct {
 	b *big.Int
 }
 
-func genGenerator(p *big.Int) *big.Int {
+func genGenerator(rand io.Reader, p *big.Int) *big.Int {
 	var G big.Int
+	var totient := p-1
+	var isGenerator = false
+	for !isGenerator {
+		G = crand.Int(rand,p)
+		//Test gcd(G,totient) = 1
+	}	
 	//TODO: All of this
 	return &G
 }
@@ -27,7 +33,9 @@ func genKeys(k int) (PubKey, PrivKey) {
 	var pubKey PubKey
 	var privKey PrivKey
 	rng := rand.New(rand.NewSource(time.Now().UnixNano())) //Not crypto secure, but will work for now
-	pubKey.p, _ = crand.Prime(rng, k)
+
+	pubKey.p, _ = crand.Prime(rng, k) //Bruh moment, we need a safe prime
+
 	// pubKey.g = genGenerator(pubKey.p)
 	privKey.b, _ = crand.Int(rng, pubKey.p)
 	// pubKey.a.Exp(pubKey.g, privKey.b, pubKey.p)
